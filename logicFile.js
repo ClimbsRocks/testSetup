e2c8fa7c6f640a867ce141d872f690b9547e9b25
ClimbsRocks <climbsbytes@gmail.com>
2016-01-13 09:37:25 -0800

    adds some more functions to test

--- logicFile.js
+++ logicFile.js
@@ -1,5 +1,17 @@
 module.exports = {
   multiply: function(a,b) {
     return a * b;
+  },
+  power: function(base, exponent) {
+    return Math.pow(base, exponent);
+  },
+  lowestCommonDenominator: function(a,b) {
+    for(var i = 2; i < Infinity; i++) {
+      // console.log(i);
+      // console.log(a,b);
+      if((a % i === 0) && (b % i === 0)) {
+        return i;
+      }
+    }
   }
 };
